517af7da4ea07dcdbf6be6ca82ea3e50e882d00c
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-11 13:02:59 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to concat 2 strings
+    """
+    return (str1 + str2)

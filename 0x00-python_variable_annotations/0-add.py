40ab9cfa04677d9d573012d55c5974bd01b2b76e
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-11 12:48:38 +0000

    add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Takes two decimals a, b and return their sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums two floating-point numbers
+    """
+    return (a + b)

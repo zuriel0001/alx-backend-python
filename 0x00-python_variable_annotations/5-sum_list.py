fee8ebdd050eb240b032787ef81017051346e7b1
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-11 13:49:37 +0000

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Sum list
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Computes the sum of a list of floating-point numbers.
+    """
+    return (sum(input_list))
